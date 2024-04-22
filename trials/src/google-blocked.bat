a45853bd2eaaebae4aa22a101aae1e4252b6b9a6
Hussam Habib <hussamh10@gmail.com>
2024-04-22 11:30:42 -0500

    googel admin API

---
+++ trials/src/google-blocked.bat
@@ -0,0 +1,30 @@
+python google-blocked.py 0
+python google-blocked.py 1
+python google-blocked.py 2
+python google-blocked.py 3
+taskkill /F /IM chrome.exe
+python google-blocked.py 4
+python google-blocked.py 5
+python google-blocked.py 6
+python google-blocked.py 7
+taskkill /F /IM chrome.exe
+python google-blocked.py 8
+python google-blocked.py 9
+python google-blocked.py 10
+python google-blocked.py 11
+python google-blocked.py 12
+taskkill /F /IM chrome.exe
+python google-blocked.py 13
+python google-blocked.py 14
+python google-blocked.py 15
+python google-blocked.py 16
+taskkill /F /IM chrome.exe
+python google-blocked.py 17
+python google-blocked.py 18
+python google-blocked.py 19
+python google-blocked.py 20
+taskkill /F /IM chrome.exe
+python google-blocked.py 21
+python google-blocked.py 22
+python google-blocked.py 23
+python google-blocked.py 24
