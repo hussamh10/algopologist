e3f1363f57220a24069041c90646d60e3773bcce
Hussam-Windows <hussamh10@gmail.com>
2024-04-22 11:42:31 -0500

    local

--- core/browser/Selenium.py
+++ core/browser/Selenium.py
@@ -119,13 +119,13 @@ class UC_single_Browser(Browser):
         instance.driver = uc.Chrome(user_data_dir=path, options=options, use_subprocess=False, version_main=122)
         sleep(4)
         monkey.GotIt()
-        sleep(2)
-        monkey.GotIt()
-        sleep(1)
-        monkey.remindMeLater()
-        sleep(3)
-        gui.hotkey('win', 'up')
-        sleep(5)
+        # sleep(2)
+        # monkey.GotIt()
+        # sleep(1)
+        # monkey.remindMeLater()
+        # sleep(3)
+        # gui.hotkey('win', 'up')
+        # sleep(5)
 
     def closeDriver(self):
         pass
